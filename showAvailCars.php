49e4ee70ed0f740c3e04ea2fbef6c37b07572099
jamesrohan <james.reckon@gmail.com>
2017-12-02 17:20:17 -0500

    Data Models for Flights, Cars, Parking
    Data Models for Flights, Cars, Parking
    
    Retrives Available Flights, Cars and Parking According to Location,
    Availability, Start and End Dates etc.

---
+++ showAvailCars.php
@@ -0,0 +1,82 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "finalproject";
+
+
+$_POST["CarType"] = "SUV";
+$_POST["Location"] = "Atlanta";
+
+
+$r = mysqli_connect($host, $user, $pass);
+
+
+//$sd = mysqli_real_escape_string($r , $_POST["StartDate"]);
+//$StartDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $sd)));
+//$ed = mysqli_real_escape_string($r, $_POST["EndDate"]);
+//$EndDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $ed)));
+
+
+
+
+
+if (!$r) {
+    echo "Could not connect to server\n";
+    trigger_error(mysqli_error($r), E_USER_ERROR);
+} else {
+    echo "Connection established\n";
+}
+
+$r2 = mysqli_select_db($r, $db);
+
+if (!$r2) {
+    echo "Cannot select database\n";
+    trigger_error(mysqli_error($r), E_USER_ERROR);
+} else {
+    echo "Database selected\n";
+}
+
+
+
+
+
+$query = 'SELECT Car_ID, 	Car_Brand_Make,	Car_Model_Name,
+                  Car_Location,Car_Year, Car_Type
+          FROM cars
+          WHERE Available_Car_Capacity > 0 AND '.'Car_Type ="'. $_POST["CarType"] .'" AND '.
+          'Car_Location ="' .$_POST["Location"]. '"';
+
+$rs = mysqli_query($r, $query);
+
+if (!$rs) {
+    echo "Could not execute query: $query";
+    trigger_error(mysqli_error($r), E_USER_ERROR);
+} else {
+    echo "Query: $query executed\n";
+}
+
+
+echo '<form action="" method= "post"><table>';
+while ($row = mysqli_fetch_assoc($rs)) {
+
+
+
+
+    echo '<tr><hr>'.'<input type="radio" name = "car_id" id="radioButton" value ="'. $row['Car_ID'] .'">'.
+                'Car Model: '. $row['Car_Model_Name']. '<br>'.
+
+                'Brand: '  .$row['Car_Brand_Make'] . '   Year: '.$row['Car_Year'].'<br>'.
+
+                'Pickup Location: '.$row['Car_Location'].'<br>'.
+
+                '<hr>'.
+        '</tr>';
+
+}
+echo "</table></form>";
+
+mysqli_close($r);
+
+?>
---
+++ showAvailFlights.php
@@ -0,0 +1,84 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "finalproject";
+
+
+$_POST["StartDate"] = "2000-11-29 01:01:30"; //new DateTime("2000-11-29 01:01:30");
+$_POST["EndDate"] = "2000-11-30 01:01:30"; //new DateTime("2000-11-30 01:01:30");
+$_POST["Departure"]= "Atlanta";
+$_POST["Destination"]= "Mexico";
+
+
+
+$r = mysqli_connect($host, $user, $pass);
+
+
+//$sd = mysqli_real_escape_string($r , $_POST["StartDate"]);
+//$StartDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $sd)));
+//$ed = mysqli_real_escape_string($r, $_POST["EndDate"]);
+//$EndDate = date('Y-m-d H:i:s', strtotime(str_replace('-', '/', $ed)));
+
+
+
+
+
+if (!$r) {
+    echo "Could not connect to server\n";
+    trigger_error(mysqli_error($r), E_USER_ERROR);
+} else {
+    echo "Connection established\n";
+}
+
+$r2 = mysqli_select_db($r, $db);
+
+if (!$r2) {
+    echo "Cannot select database\n";
+    trigger_error(mysqli_error($r), E_USER_ERROR);
+} else {
+    echo "Database selected\n";
+}
+
+
+
+
+
+$query = 'SELECT FSchedule_ID, Flight_Name, Airline_Name, Start, End_d,
+                 Depart_Location, Destination
+                 FROM flights, flight_schedule
+                  WHERE 	FlightID_FK = Flight_ID AND
+                          Depart_Location ='.'"'. $_POST["Departure"].'" AND '.
+                          'Destination = '.'"'. $_POST["Destination"] .'" AND '.
+                          'Start >="'. $_POST["StartDate"].'" AND '.
+                          'End_d <="'. $_POST["EndDate"] .'" AND '.
+                          'Current_Capacity > 0';
+
+$rs = mysqli_query($r, $query);
+
+if (!$rs) {
+    echo "Could not execute query: $query";
+    trigger_error(mysqli_error($r), E_USER_ERROR);
+} else {
+    echo "Query: $query executed\n";
+}
+
+
+echo '<form action="" method= "post"><table>';
+while ($row = mysqli_fetch_assoc($rs)) {
+
+
+    echo '<tr><hr>'.'<input type="radio" name = "flight_number" id="radioButton" value ="'. $row['FSchedule_ID'] .'">'.
+                'Flight: ' .$row['Flight_Name'].'<br>'.
+                'Departure: '  .$row['Depart_Location'] . '   Departure Time: '. $row['Start'].'<br>'.
+                'Arrival: '.$row['Destination'].'   Arrival Time: '.$row['End_d'].'<br>'.
+                'Airlines: '.$row['Airline_Name'].'<hr>'.
+        '</tr>';
+
+}
+echo "</table></form>";
+
+mysqli_close($r);
+
+?>
